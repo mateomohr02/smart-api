8f7892cf42884baf25e9b40588afdcd234213fee
Mateo Mohr <mohr.mateo@gmail.com>
2023-10-29 12:28:33 -0300

    'setting up for deploy'

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-
+require ("dotenv").config()
 const server = require('./src/app.js');
 
-server.listen(3001, () => {
-  console.log(`Server listening at 3001`);
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+  console.log(`Server listening at ${PORT}`);
 });
